990e2d0a1b193e7b371f48a9bbfd92dbfd05a5d5
Leonardo Luz <leonardoluz10@hotmail.com>
2024-07-30 12:07:23 -0300

    feat: :white_check_mark: Feat: Criando estados com funções e classes, controlando campos do formulario com estados, criando interface e reaproveitando estados.

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,8 +1,14 @@
 import style from './Botao.module.scss';
 
-export default function Botao({ children }: { children: string }) {
+export default function Botao(
+    { children, type = 'button' }
+        : { children: string, type?: "button" | "submit" | "reset" | undefined }
+) {
     return (
-        <button className={style.botao}>
+        <button
+            className={style.botao}
+            type={type}
+        >
             {children}
         </button>
     )
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import Cronometro from "../components/Cronometro"
 import Formulario from "../components/Formulario"
 import Lista from "../components/Lista"
 import styles from './App.module.scss'
+import { ITarefa } from "../types/tarefa";
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
 
   return (
     <div className={styles.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas}/>
       <Cronometro />
     </div>
   )
