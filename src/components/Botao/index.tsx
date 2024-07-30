44d4646be0010debd8118aaa0ee3cc952e2643c7
Leonardo Luz <leonardoluz10@hotmail.com>
2024-07-30 20:18:33 -0300

    feat: :white_check_mark: Feat: Foi criado uma função para finalizar tarefa no pages, inserindo checagem de class nos li, criado função regressiva e usamos o hook useEffect. finalizado curso.

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,18 @@
 import style from './Botao.module.scss';
 
+interface Props {
+    children?: React.ReactNode;
+    type?: "button" | "submit" | "reset" | undefined;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
 export default function Botao(
-    { children, type = 'button' }
-        : { children: string, type?: "button" | "submit" | "reset" | undefined }
+    { children, type = 'button', onClick }
+        : Props
 ) {
     return (
         <button
+            onClick={onClick}
             className={style.botao}
             type={type}
         >
